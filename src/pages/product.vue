5bee576ff8b22078f7de9c703b2369a1e46ef7fd
杨浩颖 <852453624@qq.com>
2020-08-19 16:27:26 +0800

    商品详情页吸顶

--- src/pages/product.vue
+++ src/pages/product.vue
@@ -1,19 +1,48 @@
 <template>
-  <div>
-    product
+  <div class="product">
+    <product-param>
+      <template v-slot:buy>
+        <button class="btn">立即购买</button>
+      </template>
+    </product-param>
+    <div class="h">123</div>
+    <div class="h">123</div>
+    <div class="h">123</div>
+    <div class="h">123</div>
+    <div class="h">123</div>
+    <div class="h">123</div>
+    <div class="h">123</div>
+    <div class="h">123</div>
+    <div class="h">123</div>
+    <div class="h">123</div>
+    <div class="h">123</div>
+    <div class="h">123</div>
+    <div class="h">123</div>
+    <div class="h">123</div>
+    <div class="h">123</div>
   </div>
   
 </template>
 
 <script>
-
+import ProductParam from './../components/ProductParam'
 
 export default {
   name:'product',
-
+  components:{
+    ProductParam
+  }
 }
 </script>
 
-<style>
-
+<style lang="scss">
+  .product{
+    .h{
+      height: 150px;
+      background-color: green;
+    }
+    button{
+      margin-left:10px;
+    }
+  }
 </style>
